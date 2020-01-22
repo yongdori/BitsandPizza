c7a6ab29e06946cd0eea5c55459d6a8f4a688e27
Steve Lee <yongdori@gmail.com>
2020-01-21 21:12:44 -0500

    Initial commit

---
+++ app/src/main/java/com/hfad/bitsandpizza/MainActivity.java
@@ -0,0 +1,17 @@
+package com.hfad.bitsandpizza;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+}
---
+++ app/src/main/java/com/hfad/bitsandpizza/OrderActivity.java
@@ -0,0 +1,14 @@
+package com.hfad.bitsandpizza;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class OrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+    }
+}
